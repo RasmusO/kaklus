f88671b631969df8978f0c3fff9e698fa20a42f4
Sander <LQKI@users.noreply.github.com>
2013-11-19 18:29:47 +0200

    Update kaklus.py

--- kaklus.py
+++ kaklus.py
@@ -2,7 +2,7 @@ import pygame
 
 class Player(pygame.sprite.Sprite): 
    
-    # Liikumise kiirus
+    # Liikumine
     xchange = 0
     ychange = 0
  
@@ -16,20 +16,24 @@ class Player(pygame.sprite.Sprite):
     sprite_row = 0
     sprite_column = 0
     frame_count = 0
+    scale = 3
+    direction = "left"
     
-    def __init__(self,x,y): 
+    def __init__(self,x,y,spritesheet): 
         pygame.sprite.Sprite.__init__(self) 
         
            
-        self.image = pygame.image.load("sprites.gif")
+        self.image = pygame.transform.scale(pygame.image.load(spritesheet), (548*self.scale,1081*self.scale))
         self.rect = self.image.get_rect() 
         self.rect.x = x
         self.rect.y = y
     
     #joonistame õige raami spritesheetist
     def draw(self, target_surface):
-        patch_rect = (self.sprite_column * 50+273, self.sprite_row*50, 50, 85)
+        patch_rect = (self.sprite_column * 45*self.scale + 7*self.scale, self.sprite_row*92*self.scale, 50*self.scale, 92*self.scale)
         target_surface.blit(self.image, (self.rect.x, self.rect.y), patch_rect)
+
+
 
     # uuendame tegelase asukohta
     def update(self): 
@@ -42,9 +46,9 @@ class Player(pygame.sprite.Sprite):
             self.xchange = 0
             self.rect.x = 0
 
-        if self.rect.x >= 785 and self.xchange != 0:
+        if self.rect.x >= 1000-50*self.scale and self.xchange != 0:
             self.xchange = 0
-            self.rect.x = 785
+            self.rect.x = 1000-50*self.scale
 
  
         # vertikaalne liikumine
@@ -67,11 +71,11 @@ class Player(pygame.sprite.Sprite):
          
     # gravitatsioon hüppamise jaoks
     def gravity(self):
-        self.ychange += .85
+        self.ychange += .55
         # kontroll kas tegelane on maa peal või õhus
-        if self.rect.y >= 500 and self.ychange >= 0:
+        if self.rect.y >= 300 and self.ychange >= 0:
             self.ychange = 0
-            self.rect.y = 500
+            self.rect.y = 300
             self.jumps = 2
  
     # hüppamine (tegin double jumpiga et oleks huvitavam)
@@ -95,27 +99,36 @@ class Player(pygame.sprite.Sprite):
         print(other.hp)
  
 pygame.init() 
-window=[800,600] 
+window=[1000,600] 
 screen=pygame.display.set_mode(window) 
 pygame.display.set_caption("Kaklus") 
 all_sprites = pygame.sprite.Group()
+
+    
+frame0 = pygame.image.load("frame_000.gif").convert()
+frame1 = pygame.image.load("frame_001.gif").convert()
+frame2 = pygame.image.load("frame_002.gif").convert()
+frame3 = pygame.image.load("frame_003.gif").convert()
+frame4 = pygame.image.load("frame_004.gif").convert()
+frame5 = pygame.image.load("frame_005.gif").convert()
+frame6 = pygame.image.load("frame_006.gif").convert()
+frame7 = pygame.image.load("frame_007.gif").convert()
    
-# loon 2 tegelast kasutades Player klassi ning määran nende asukohad
-player1 = Player(100,100)
-player1.rect.x = 500
-player1.rect.y = 485
+# loon 2 tegelast kasutades Player klassi
+player1 = Player(500,285,"surge2.png")
+
+
 all_sprites.add(player1)
 
-player2 = Player(100,100)
-player2.rect.x = 100
-player2.rect.y = 485
+player2 = Player(100,285,"black2.png")
+
 all_sprites.add(player2)
 
 done = False
 clock = pygame.time.Clock() 
 
-
-
+frame = 0
+background = frame0
 
 # main loop
 while not done: 
@@ -160,11 +173,34 @@ while not done:
     player1.update() 
     player2.gravity()
     player2.update()
-      
+    
+    if frame == 48:
+        frame = 0
+    else:
+        frame += 1
+    
+    if frame == 0:
+        background = frame0
+    elif frame == 6:
+        background = frame1
+    elif frame == 12:
+        background = frame2
+    elif frame == 18:
+        background = frame3
+    elif frame == 24:
+        background = frame4
+    elif frame == 30:
+        background = frame5
+    elif frame == 36:
+        background = frame6
+    elif frame == 42:
+        background = frame7
+
+
     screen.fill([0,0,0])
    
 
-   
+    screen.blit(background, (0,0))
     for sprite in all_sprites:
         sprite.draw(screen)
 
